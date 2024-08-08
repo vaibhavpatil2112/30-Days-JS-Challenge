6e6c9bc410355f866e013c284052317cd3f28d63
vaibhav patil~ <vaibhavpatil211299@gmail.com$ git config --global user.name vaibhav>
2024-08-08 18:51:46 +0530

    First commit

---
+++ Day1/app.js
@@ -0,0 +1,57 @@
+// Activity 1 Variable declaration
+
+var vaibhav = 5;
+console.log(vaibhav);
+
+let str = "Vaibhav";
+console.log(str);
+
+// Activity 2 Constant declaration
+
+const x = true;
+console.log(x);
+
+// Activity 3 data types      BNUNBSS
+
+const x1 = false
+console.log(typeof x1);
+
+const T = null
+console.log(typeof T); // object
+
+let o;
+console.log(typeof o);
+
+const num = 8
+console.log(typeof num);
+
+const nums = 6785608439475640685;
+console.log(typeof nums);               // Number
+
+const strr = "Piyush"
+console.log(typeof strr);
+
+const sys = Symbol
+console.log(typeof sys);  // Function
+
+
+// Activity 4 Reassigning
+
+let numm = 8
+// console.log(numm);
+numm = 10
+console.log(numm);
+
+const hey = "HI"
+hey = "Hello"
+
+console.log(hey);    // TypeError: Assignment to constant variable.
+
+
+
+
+
+
+
+
+
